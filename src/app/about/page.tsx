d4591ffbe37df48bd1cc3ab6df9f1c038afba318
iAdityaDev <adityadevsingh16@gmail.com>
2026-09-04 16:29:49 +0530

    website member data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -413,13 +413,13 @@ const About = () => {
                 linkedinLink: "https://www.linkedin.com/in/mahir-pandey-184a63287"
               },
 
-              // {
-              //   circleImageSrc: "/images/aman.webp",
-              //   name: "Aman Kumar Yadav (@hyper_engine)",
-              //   description: "ROS | Electronics",
-              //   githubLink: "https://github.com/PiX8iT",
-              //   linkedinLink: "https://www.linkedin.com/in/aman-kumar-yadav-b1b65a381"
-              // },
+              {
+                circleImageSrc: "/images/aman.webp",
+                name: "Aman Kumar Yadav (@hyper_engine)",
+                description: "ROS | Electronics",
+                githubLink: "https://github.com/PiX8iT",
+                linkedinLink: "https://www.linkedin.com/in/aman-kumar-yadav-b1b65a381"
+              },
 
               //               {
               //   circleImageSrc: "/images/khushal.webp",
