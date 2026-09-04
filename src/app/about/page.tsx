cc8233cf0ca8a64f43e203ae4d539379460fbbc0
iAdityaDev <adityadevsingh16@gmail.com>
2026-09-04 16:25:51 +0530

    website member data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -429,14 +429,14 @@ const About = () => {
               //   linkedinLink: "https://www.linkedin.com/in/khushal-jamdegni-4b3a57313/"
               // },
 
-              // {
-              //   circleImageSrc: "/images/vanshbhardwaj.webp",
-              //   name: "Vansh Bhardwaj (@vanshbhardwaj 
-              //   description: "Core | Electronics",
-              //   instagramLink: "https://www.instagram.com/vansh_vb07?igsi=emJ0MGV5YXc3aXN4",
-              //   githubLink: "https://github.com/Vansh-77",
-              //   linkedinLink: "https://www.linkedin.com/in/vansh-bhardwaj-2102b7328?utm_source=share_via&utm_content=profile&utm_medium=member_android"
-              // },
+              {
+                circleImageSrc: "/images/vanshbhardwaj.webp",
+                name: "Vansh Bhardwaj (@vanshbhardwaj 
+                description: "Core | Electronics",
+                instagramLink: "https://www.instagram.com/vansh_vb07?igsi=emJ0MGV5YXc3aXN4",
+                githubLink: "https://github.com/Vansh-77",
+                linkedinLink: "https://www.linkedin.com/in/vansh-bhardwaj-2102b7328?utm_source=share_via&utm_content=profile&utm_medium=member_android"
+              },
 
               // {
               //   circleImageSrc: "/images/sakshammalik.webp",
